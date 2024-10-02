fcb6c360e54aeeb7356b3f91c4d6b4423fd3c377
MobbyYT <mobby2004yt@gmail.com>
2024-10-02 11:54:33 +0300

    This is implemented react, but it's not correct at all Need to be reworked completely Bad implementation of it.

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DebtForm from "./DebtForm";
+import LanguageSwitcher from "./LanguageSwitcher";
+
+function App() {
+    return (
+        <div>
+            <LanguageSwitcher />
+            <DebtForm />
+        </div>
+    );
+}
+
+export default App;
--- scripts.js
+++ scripts.js
@@ -216,3 +216,26 @@ function saveOrderToStorage() {
     });
     localStorage.setItem("debts", JSON.stringify(debts));
 }
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from ".App";
+
+ReactDom.render(<App />, document.getElementsById("root"));
+
+import { useTranslation } from "react-i18next";
+
+const DebtForm = () => {
+    const { t } = useTranslation();
+
+    return (
+        <div>
+            <input type="text" id="name" placeholder={t("debt.name")} required />
+            <input type="text" id="date" placeholder={t("debt.date")} required />
+            <input type="text" id="debtor" placeholder={t("debt.debtor")} required />
+            <button id="submit-debt">{t("debt.addDebt")}</button>
+        </div>
+    );
+};
+
+export default DebtForm;
